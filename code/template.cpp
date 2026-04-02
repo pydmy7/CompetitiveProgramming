1ef0c0fe0e5c7bd64ad254558e9cfac5dc3992a1
user1 <pydmy7@gmail.com>
2026-04-02 11:52:49 +0800

    backup

---
+++ code/template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+void solve() {
+    
+}
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
